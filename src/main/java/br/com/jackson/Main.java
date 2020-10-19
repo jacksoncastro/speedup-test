f9ca5b5e33998320f6f2423133a82acf54ada743
Jackson Castro <jackson.castro@sefin.fortaleza.ce.gov.br>
2020-10-19 16:49:23 -0300

    Change timezone

--- src/main/java/br/com/jackson/Main.java
+++ src/main/java/br/com/jackson/Main.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -66,7 +67,9 @@ public class Main {
 	}
 
 	private String getDate() {
-		return new SimpleDateFormat(PATTERN_DATE).format(new Date());
+		SimpleDateFormat format = new SimpleDateFormat(PATTERN_DATE);
+		format.setTimeZone(TimeZone.getTimeZone("America/Fortaleza"));
+		return format.format(new Date());
 	}
 
 	private void test(Scenarie scenarie, String date, int round) {
